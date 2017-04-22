10e29739cb6ce46321bcf887c373ec0dc9f4ad13
Jared babinec <jbabinec1@gmail.com>
2017-04-21 20:47:04 -0400

    Add files via upload

---
+++ jQuery generator/js/index.js
@@ -0,0 +1,73 @@
+ $('[name="cand_no" ]').on('change', function() {
+  // Not checking for Invalid input
+  if (this.value != '') {
+    var val = parseInt(this.value, 10);
+    
+
+    
+    ///Adds rows to studentTable with each click
+    
+    var times = $("input").val();
+  	$("tr").slice(2).remove();
+    for (var i = 0; i < times; i++) {
+    
+      
+         	$("#myTable tbody").append("<tr><td > row 1</td><td> row 2 </td></tr>");
+       
+     
+    }
+    
+    
+    
+    
+    
+    
+    ///Click add button add count number + table
+    $(function() {
+      $('#add').bind('click', function() {
+        $('#mytbody');
+        var count = $('#mytbody').children('tr').length;
+        $('#counter').html(count);
+        
+        var count2 = $('#mytbody').children('tr').length;
+        $('#countagain').html(count2);
+        
+        
+        
+        ///Adding coder name 
+        var name = $("#tname").val();
+        $('#aname').html(name);
+      });
+    });
+    
+    
+    
+    /// Shade Every Other Row 
+    
+    $("#shadeRow").click(function() {
+    $('tr:nth-child(3n)').toggleClass("highlight");
+});
+    
+    
+    
+    //Underline every 10 rows 
+    $("#underLine").click(function() {
+ 
+$('tr:nth-child(10n)').toggleClass("underline");
+
+});
+   
+
+     //Reset Rows Button 
+   
+    $("#resetBtn").click(function() {
+    
+   $('#myTable tr:not(:first)').remove();
+   $('.hidecounter').remove();
+      
+      
+    }); 
+  }
+ }); 
+    
+    
